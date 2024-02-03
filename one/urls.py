cc8f34f5af2956fadd9eb6384eb563d9664b20c3
Sandeep Gaur <neuralpietechnology@gmail.com>
2024-02-03 15:04:56 +0530

    workable draft

--- one/urls.py
+++ one/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('event_calendar/', views.event_calendar, name='event_calendar'),
+    path('news/', views.news, name='news'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('contact/', views.contact, name='contact'),
+    path('member_page/', views.member_page, name='member_page'),
+
 ]
--- one/views.py
+++ one/views.py
@@ -4,3 +4,24 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'one/index.html', {})
 # Create your views here.
+def sample(request):
+    return render(request, 'one/sample.html', {})
+
+def about(request):
+    return render(request, 'one/about.html', {})
+
+def event_calendar(request):
+    return render(request, 'one/event_calendar.html', {})
+
+def news(request):
+    return render(request, 'one/news.html', {})
+
+def gallery(request):
+    return render(request, 'one/gallery.html', {})
+
+def contact(request):
+    return render(request, 'one/contact.html', {})
+
+
+def member_page(request):
+    return render(request, 'one/member_page.html', {})
